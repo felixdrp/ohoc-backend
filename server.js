71a28ef3e2406aa63947951310721cc7c7ad8871
felixdrp <felixdrp@gmail.com>
2016-12-11 22:13:08 +0000

    Upload multimedia files.

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var bodyParser = require('body-parser')
 var formidable = require('formidable')
 var util = require('util');
 var dbDriver = require('./src/api/db-driver').default;
+var fs = require('fs')
 
 
 
@@ -11,6 +12,7 @@ var app = express();
 app.use(bodyParser.json())
 
 app.use(express.static('public'))
+app.use('/multimedia', express.static('data'))
 
 app.get('/', (req, res) => {
   res.writeHead(200, {'content-type': 'text/html'});
@@ -135,29 +137,55 @@ app.route('/api/setRecord/:recordId')
   })
 
 app.route('/api/record/upload/:recordId')
-  .put(async (req, res) => {
+  .post(async (req, res) => {
     try {
       var form = new formidable.IncomingForm();
       form.uploadDir = __dirname + '/data'
       form.keepExtensions = true;
 
-      form.on('end', (err, fields, files) => {
-        res.writeHead(200, {'content-type': 'text/plain'});
-        res.write('received upload:\n\n');
-        res.end(util.inspect({fields: fields, files: files}));
-      })
+      // Events sorted by FIFO first in (list) first out (executed)
 
-      form.on('file', async (name, file) => {
-        console.log(name)
-        console.log(file)
-      })
+      // form.on('file', async (name, file) => {
+      //   // console.log('1 > ' + Date.now())
+      //   console.log(name)
+      //   console.log(file)
+      // })
+      //
+      // form.on('end', (err) => {
+      //   console.log('2 > ' + Date.now())
+      // })
 
       // Process the files
       form.parse(req, function(err, fields, files) {
+        let recordId = req.params.recordId
+        let uploads = []
+        // console.log('3 > ' + Date.now())
+
         // console.log('parse!!')
         // console.log(err)
         // console.log(fields)
         // console.log(files)
+
+        // Process files:
+        // rename
+        // Future use async fs.rename()
+
+        // console.log(form)
+        // console.log(files)
+        // console.log(form.openedFiles.constructor.name)
+        for ( let {path, name, type} of form.openedFiles ) {
+          let newPath =  recordId + '_' + name
+          // console.log(path, name, type)
+          fs.renameSync( path, __dirname + '/data/' + newPath )
+          uploads.push({
+            src: newPath,
+            type
+          })
+
+        }
+
+        res.writeHead(200, {'content-type': 'application/json'});
+        res.end( JSON.stringify({ upload: { files: uploads } }) );
       });
 
     } catch(error) {
@@ -168,41 +196,41 @@ app.route('/api/record/upload/:recordId')
 
 app.use(async function(req, res) {
   console.log(req.headers)
-  if (req.url == '/upload' && req.method.toLowerCase() == 'post') {
-    console.log('uploading file')
-    // parse a file upload
-    try {
-      var form = new formidable.IncomingForm();
-      form.uploadDir = __dirname + '/data'
-      form.keepExtensions = true;
-
-      form.on('end', (err, fields, files) => {
-        res.writeHead(200, {'content-type': 'text/plain'});
-        res.write('received upload:\n\n');
-        res.end(util.inspect({fields: fields, files: files}));
-      })
-
-      form.on('file', async (name, file) => {
-        console.log(name)
-        console.log(file)
-      })
-
-      form.parse(req, function(err, fields, files) {
-        console.log('parse!!')
-        console.log(err)
-        console.log(fields)
-        console.log(files)
-      });
-    } catch(error) {
-      console.log('ERROR: ' + error)
-    }
-
-    return;
-  }
+  // if (req.url == '/upload' && req.method.toLowerCase() == 'post') {
+  //   console.log('uploading file')
+  //   // parse a file upload
+  //   try {
+  //     var form = new formidable.IncomingForm();
+  //     form.uploadDir = __dirname + '/data'
+  //     form.keepExtensions = true;
+  //
+  //     form.on('end', (err, fields, files) => {
+  //       res.writeHead(200, {'content-type': 'text/plain'});
+  //       res.write('received upload:\n\n');
+  //       res.end(util.inspect({fields: fields, files: files}));
+  //     })
+  //
+  //     form.on('file', async (name, file) => {
+  //       console.log(name)
+  //       console.log(file)
+  //     })
+  //
+  //     form.parse(req, function(err, fields, files) {
+  //       console.log('parse!!')
+  //       console.log(err)
+  //       console.log(fields)
+  //       console.log(files)
+  //     });
+  //   } catch(error) {
+  //     console.log('ERROR: ' + error)
+  //   }
+  //
+  //   return;
+  // }
   // show a file upload form
   res.writeHead(200, {'content-type': 'text/html'});
   res.end(
-    '<form action="/upload" enctype="multipart/form-data" method="post">'+
+    '<form action="/api/record/upload/24" enctype="multipart/form-data" method="post">'+
     '<input type="text" name="title"><br>'+
     '<input type="file" name="upload" multiple="multiple"><br>'+
     '<input type="submit" value="Upload">'+
